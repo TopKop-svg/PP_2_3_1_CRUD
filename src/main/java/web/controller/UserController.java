27eeadbf29389ac714707cb27482bb676782c9b0
TopKop-svg <loookintome@gmail.com>
2024-03-08 17:38:44 +0900

    save user with addUser need fix

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -3,10 +3,7 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.repository.UserRepository;
 
@@ -17,29 +14,39 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/users")
-    public String getUsers(Model model) {
-        List<User> users = userRepository.findAll();
-        model.addAttribute("users", users);
-        return "users";
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
-    @PostMapping("/users")
+    @GetMapping("/")
+    public String userTable(Model model) {
+        List<User> users = userRepository.findAll();
+        model.addAttribute("users", users);
+        return "new";
+    }
+
+    /*@GetMapping("/adduser")
+    public String addUserPage(Model model) {
+        return "adduser";
+    }*/
+
+    @PostMapping("adduser")
     public String addUser(@RequestParam String name, @RequestParam String lastname, @RequestParam String email) {
         User user = new User();
         user.setName(name);
         user.setLastName(lastname);
         user.setMail(email);
         userRepository.save(user);
-        return "redirect:/users";
+        return "adduser";
     }
 
     @PostMapping("/users/{id}/delete")
     public String deleteUser(@PathVariable Long id) {
         userRepository.deleteById(id);
-        return "redirect:/users";
+        return "new";
     }
-
+/*
     @PostMapping("/users/{id}/update")
     public String updateUser(@PathVariable Long id, @RequestParam String name, @RequestParam String lastname, @RequestParam String email) {
         User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
@@ -47,6 +54,6 @@ public class UserController {
         user.setLastName(lastname);
         user.setMail(email);
         userRepository.save(user);
-        return "redirect:/users";
-    }
+        return "redirect:/index";
+    }*/
 }
