0930936d7b200e01656e9cacb48942f6bad2aa4d
TopKop-svg <loookintome@gmail.com>
2024-03-08 11:16:19 +0900

    add db MySQL

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -3,6 +3,10 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import web.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,14 +14,18 @@ import java.util.List;
 @Controller
 public class HelloController {
 
-	@GetMapping(value = "/")
-	public String printWelcome(ModelMap model) {
-		List<String> messages = new ArrayList<>();
-		messages.add("Hello!");
-		messages.add("I'm Spring MVC application");
-		messages.add("5.2.0 version by sep'19 ");
-		model.addAttribute("messages", messages);
-		return "index";
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	@ResponseBody
+	public String getUser(ModelMap model) {
+		return "createMockUser()";
+	}
+
+	private User createMockUser() {
+		User user = new User();
+		user.setId(Long.valueOf(1));
+		user.setName("Alex");
+		user.setMail("loookintome@gmail.com");
+		return user;
 	}
 	
 }
---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,57 @@
+package web.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    @Column(name = "last_name", nullable = false, length = 20)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, length = 20)
+    private String email;
+
+    public User() {}
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setMail(String email) {
+        this.email = email;
+    }
+}
