d2344dd80d205114ada092d40ab2c764fe9f23ec
TopKop-svg <loookintome@gmail.com>
2024-03-15 00:27:38 +0900

    work version, but without debug

--- src/main/java/web/config/JpaConfig.java
+++ src/main/java/web/config/JpaConfig.java
@@ -1,8 +1,11 @@
 package web.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -12,24 +15,33 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan("web")
 @EnableJpaRepositories(basePackages = "web.repository")
+@PropertySource("classpath:db.properties")
 public class JpaConfig {
+    private final Environment env;
+
+    public JpaConfig(Environment env) {
+        this.env = env;
+    }
+
     //TODO сделать через проперти
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/usersdb?createDatabaseIfNotExist=true");
-        dataSource.setUsername("root");
-        dataSource.setPassword("Akame_ga_ki11");
+        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
         return dataSource;
     }
 
-    @Bean
+
+    /*@Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource());
@@ -37,12 +49,45 @@ public class JpaConfig {
         em.setPersistenceUnitName("myPersistenceUnit");
         em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
         return em;
-    }
+    }*/
 
-    @Bean
+    /*@Bean
     public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(emf);
         return transactionManager;
+    }*/
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }
+
+
+     @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(getEntityPackages());
+        em.setPersistenceUnitName("myPersistenceUnit");
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        jpaProperties.put("hibernate.show_sql", true);
+        jpaProperties.put("hibernate.format_sql", true);
+        jpaProperties.put("hibernate.hbm2ddl.auto", "update");
+        jpaProperties.put("hibernate.connection.charSet", "UTF-8");
+        em.setJpaProperties(jpaProperties);
+
+        return em;
+    }
+
+    private String[] getEntityPackages() {
+        String dbEntityPackage = env.getRequiredProperty("db.entity.package");
+        String[] packages = {dbEntityPackage, "web.model"};
+        return packages;
     }
 }
